b054b284f5d4b3fd7f73ba9276738ce099d826d7
839168_Priyanka <priyankadewangan7775@gmail.com>
2022-07-28 11:47:52 +0000

    Auto saving changes from StackRoute workbench on Thu Jul 28 2022 11:47:42 GMT+0000 (Coordinated Universal Time)

---
+++ ReverseArrayDemo.java
@@ -0,0 +1,13 @@
+class ReverseArrayDemo{
+	public static void main(String[] args){
+		int[] arr = {23,34,43,56,67,43,35};
+	}
+
+	public int[] getReverseArray(int[] array){
+		for(int i=0;i<array.length;i++){
+			int temp=(array[])%10;
+			reverseArray[]=reverseArray[]*10 +
+		}
+		
+	}
+}
