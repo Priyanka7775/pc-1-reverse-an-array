68fd7d8f9df16aae61623d1eb3b2203b2d3073e0
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-08-03 14:33:51 +0530

    Sprint 2

--- src/main/java/com/jap/ReverseArrayDemo.java
+++ src/main/java/com/jap/ReverseArrayDemo.java
@@ -3,21 +3,23 @@ package com.jap;
 public class ReverseArrayDemo {
 
     public static void main(String[] args) {
-
-        //Declare and initialize values of an array.
-
-        //Create an object of the class ReverseArrayDemo.
-
-       //Call the method getReverseArray and pass the array as parameter
-
+        int[] array = {23,34,43,56,67,78,43,35};
+        ReverseArrayDemo reverseArrayDemo= new ReverseArrayDemo();
+        int [] reverseArray= reverseArrayDemo.getReverseArray(array);
 
     }
+
 
     //Write the logic to reverse the given array inside the below
     // method and return the reverse array.
     public int[] getReverseArray(int [] array){
 
-        return new int[0];
+
+        for(int i=array.length-1;i>=0;i--){
+            System.out.print(array[i]+",");
+        }
+
+        return array;
 
 
     }
